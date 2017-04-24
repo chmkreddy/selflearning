b91b3c2b8bc61eef5b46e385781dffc18d15cb7f
chmkreddy <chmkreddy@gmail.com>
2017-04-24 20:07:43 +0530

    Updated the code
    removed for loop

--- TrasnportVacation.js
+++ TrasnportVacation.js
@@ -1,14 +1,9 @@
 function rentalCarCost(d) {
-  var sum = 0; 
-for (i = 1; i <= d; i++) {
-    sum += 40;
-}
+  var sum = d * 40; 
+  
 if (d >= 7) {
 sum = sum - 50;
 }
 else if (d >= 3) {
 sum = sum - 20;
-}
-
-return sum;
 }
