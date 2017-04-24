637f18ba1e71b5aa6811316edccea366bbdce1dd
chmkreddy <chmkreddy@gmail.com>
2017-04-24 20:11:02 +0530

    Updated the code for empty check

--- GetMiddle.js
+++ GetMiddle.js
@@ -1,11 +1,17 @@
 function getMiddle(s)
 {
+if (s.length > 0) { 
   if (s.length % 2 == 1) {
-  //odd
- return s.charAt(Math.trunc(s.length/2))
+	//odd
+ return s.charAt(Math.trunc(s.length/2));
 }
 else {
- //even  
-   return s.substr(s.length/2 - 1,2)
+ //even	
+   return s.substr(s.length/2 - 1,2);
 }
+}
+else
+ { 
+   return "";
+ }
 }
