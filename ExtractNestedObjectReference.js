e23a980563afc25b395afb0cea578f74f66afb58
chmkreddy <chmkreddy@gmail.com>
2017-05-09 16:54:02 +0530

    Create ExtractNestedObjectReference.js

---
+++ ExtractNestedObjectReference.js
@@ -0,0 +1,11 @@
+Object.prototype.hash = function(string) {
+  var obj=this;
+  var a = string.split(".");
+  a.forEach(function(i){
+    if(obj)
+      {
+        obj=obj[i];
+      }
+    });
+  return obj;
+}
