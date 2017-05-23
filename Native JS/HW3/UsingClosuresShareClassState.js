655dc8887fd90a78438c0c200440d10955cb1376
chmkreddy <chmkreddy@gmail.com>
2017-05-23 11:44:37 +0530

    Update UsingClosuresShareClassState.js

--- Native JS/HW3/UsingClosuresShareClassState.js
+++ Native JS/HW3/UsingClosuresShareClassState.js
@@ -1,28 +1,38 @@
 // Let's make a Cat constructor!
-var totalWeight = 0, count = 0;
+var totalWeight = 0,
+    count = 0;
 
-var Cat = (function (name,weight) {
-  if (name == undefined && weight == undefined) {
-  throw "error";
-  }
-  else
-  {
-  this._name = name;
-  this._weight = weight;
-  
-  count++;
-  totalWeight += this._weight;
-  
- Object.defineProperty(this,"name",{
-    get: function() { return this._name; },
-    set: function(value) { this._name = value; },
-  });
- Object.defineProperty(this,"weight",{
-    get: function() { return this._weight; },
-    set: function(value) { totalWeight = totalWeight - this._weight + value; this._weight = value;   },
-  });
-  }
+var Cat = (function(name, weight) {
+    if (name == undefined && weight == undefined) {
+        throw "error";
+    } else {
+        this._name = name;
+        this._weight = weight;
+
+        count++;
+        totalWeight += this._weight;
+
+        Object.defineProperty(this, "name", {
+            get: function() {
+                return this._name;
+            },
+            set: function(value) {
+                this._name = value;
+            },
+        });
+        Object.defineProperty(this, "weight", {
+            get: function() {
+                return this._weight;
+            },
+            set: function(value) {
+                totalWeight = totalWeight - this._weight + value;
+                this._weight = value;
+            },
+        });
+    }
 
 });
 
-Cat.averageWeight = function() {  return count > 0 ? totalWeight/count : 0; };
+Cat.averageWeight = function() {
+    return count > 0 ? totalWeight / count : 0;
+};
