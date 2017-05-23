b80f450959a12922d855c52c8b26ec3880b96ad5
chmkreddy <chmkreddy@gmail.com>
2017-05-23 11:40:56 +0530

    Update ChainAddingFunction.js

--- Native JS/HW2/ChainAddingFunction.js
+++ Native JS/HW2/ChainAddingFunction.js
@@ -1,6 +1,10 @@
-function add(n){
-  // Let the currying begin!
- function innerFunction(x) { return add(n + x); };
- innerFunction.valueOf = function() {return n;}
- return innerFunction;
+function add(n) {
+    // Let the currying begin!
+    function innerFunction(x) {
+        return add(n + x);
+    };
+    innerFunction.valueOf = function() {
+        return n;
+    }
+    return innerFunction;
 }
