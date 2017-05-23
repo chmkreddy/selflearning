636c08f562b73c0a3c1234c3119b0fdbfde9a02d
chmkreddy <chmkreddy@gmail.com>
2017-05-23 11:40:23 +0530

    Update CalculatingWithFunctions.js

--- Native JS/HW2/CalculatingWithFunctions.js
+++ Native JS/HW2/CalculatingWithFunctions.js
@@ -1,23 +1,77 @@
-function calc(val,func) {
-if (func == undefined) {
- return val;
- }
- else
- return func(val);
+function calc(val, func) {
+
+    if (func == undefined) {
+
+        return val;
+
+    } else
+
+        return func(val);
+
 }
 
-function zero(func) { return calc(0,func); }
-function one(func) { return calc(1,func); }
-function two(func) { return calc(2,func); }
-function three(func) {return calc(3,func); }
-function four(func) {return calc(4,func); }
-function five(func) {return calc(5,func); }
-function six(func) {return calc(6,func); }
-function seven(func) {return calc(7,func); }
-function eight(func) {return calc(8,func); }
-function nine(func) {return calc(9,func); }
+function zero(func) {
+    return calc(0, func);
+}
 
-function plus(val2) { return function(val1) { return val1 + val2 }; }
-function minus(val2) { return function(val1) { return val1 - val2 }; }
-function times(val2) {return function(val1) { return val1 * val2 }; }
-function dividedBy(val2) {return function(val1) { return val1 / val2 }; }
+function one(func) {
+    return calc(1, func);
+}
+
+function two(func) {
+    return calc(2, func);
+}
+
+function three(func) {
+    return calc(3, func);
+}
+
+function four(func) {
+    return calc(4, func);
+}
+
+function five(func) {
+    return calc(5, func);
+}
+
+function six(func) {
+    return calc(6, func);
+}
+
+function seven(func) {
+    return calc(7, func);
+}
+
+function eight(func) {
+    return calc(8, func);
+}
+
+function nine(func) {
+    return calc(9, func);
+}
+
+
+
+function plus(val2) {
+    return function(val1) {
+        return val1 + val2
+    };
+}
+
+function minus(val2) {
+    return function(val1) {
+        return val1 - val2
+    };
+}
+
+function times(val2) {
+    return function(val1) {
+        return val1 * val2
+    };
+}
+
+function dividedBy(val2) {
+    return function(val1) {
+        return val1 / val2
+    };
+}
