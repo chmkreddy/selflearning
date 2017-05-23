2dac3b8306abbfc74f02e865b494deafad5a7359
chmkreddy <chmkreddy@gmail.com>
2017-05-23 11:43:13 +0530

    Update ExtractNestedObjectReference.js

--- Native JS/HW3/ExtractNestedObjectReference.js
+++ Native JS/HW3/ExtractNestedObjectReference.js
@@ -1,11 +1,10 @@
 Object.prototype.hash = function(string) {
-  var obj=this;
-  var a = string.split(".");
-  a.forEach(function(i){
-    if(obj)
-      {
-        obj=obj[i];
-      }
+    var obj = this;
+    var a = string.split(".");
+    a.forEach(function(i) {
+        if (obj) {
+            obj = obj[i];
+        }
     });
-  return obj;
+    return obj;
 }
