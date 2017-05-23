cf28779aa410f96dfb5b8f74d17a26caec1f5923
chmkreddy <chmkreddy@gmail.com>
2017-05-23 11:38:08 +0530

    Update GetMiddle.js

--- Native JS/HW1/GetMiddle.js
+++ Native JS/HW1/GetMiddle.js
@@ -1,17 +1,17 @@
 function getMiddle(s)
 {
-if (s.length > 0) { 
-  if (s.length % 2 == 1) {
-	//odd
- return s.charAt(Math.trunc(s.length/2));
-}
-else {
- //even	
-   return s.substr(s.length/2 - 1,2);
-}
-}
-else
- { 
-   return "";
- }
+    var middleChar = "";
+    if (s.length > 0) {
+        if (s.length % 2 == 1) {
+            //odd
+            middleChar = s.charAt(Math.trunc(s.length / 2));
+        } else {
+            //even	
+            middleChar = s.substr(s.length / 2 - 1, 2);
+        }
+    } else
+    {
+        middleChar = "";
+    }
+    return middleChar;
 }
