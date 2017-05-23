e69e3e0e2e27dd525aa4370d195d31982ec52974
chmkreddy <chmkreddy@gmail.com>
2017-05-23 11:39:00 +0530

    Update TrasnportVacation.js

--- Native JS/HW1/TrasnportVacation.js
+++ Native JS/HW1/TrasnportVacation.js
@@ -1,17 +1,9 @@
 function rentalCarCost(d) {
-
-  var sum = d * 40; 
-  
-if (d >= 7) {
-
-sum = sum - 50;
-
-}
-
-else if (d >= 3) {
-
-sum = sum - 20;
-
-}
-return sum;
+    var sum = d * 40;
+    if (d >= 7) {
+        sum = sum - 50;
+    } else if (d >= 3) {
+        sum = sum - 20;
+    }
+    return sum;
 }
